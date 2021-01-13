ca1e14dffed2577c56b3c91ea88ee2d2a5e3f75f
zyy1225 <1742775538@qq.com>
2021-01-13 20:20:53 +0800

    更新

---
+++ websys.com/application/common.php
@@ -0,0 +1,49 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2016 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 流年 <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+// 应用公共文件
+
+/**
+ * 数组转树
+ * @param type $list
+ * @param type $root
+ * @param type $pk
+ * @param type $pid
+ * @param type $child
+ * @return type
+ */
+function list_to_tree($list, $root = 0, $pk = 'id', $pid = 'parentid', $child = '_child') {
+    // 创建Tree
+    $tree = array();
+    if (is_array($list)) {
+        // 创建基于主键的数组引用
+        $refer = array();
+        foreach ($list as $key => $data) {
+            $refer[$data[$pk]] = &$list[$key];
+        }
+        foreach ($list as $key => $data) {
+            // 判断是否存在parent
+            $parentId = 0;
+            if (isset($data[$pid])) {
+                $parentId = $data[$pid];
+            }
+            if ((string) $root == $parentId) {
+                $tree[] = &$list[$key];
+            } else {
+                if (isset($refer[$parentId])) {
+                    $parent = &$refer[$parentId];
+                    $parent[$child][] = &$list[$key];
+                }
+            }
+        }
+    }
+    return $tree;
+}
